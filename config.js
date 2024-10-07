f2cf7e30b0880fe6987d64d92f9eae5402811ced
phatdatle373 <136218473+phatdatle373@users.noreply.github.com>
2024-10-07 19:36:35 +0700

    upload

---
+++ config.js
@@ -0,0 +1,21 @@
+//====Nhập Thông Tin Của Bạn Vào Đây==============
+
+//Link Ảnh Background
+ImgLink_Background = "https://cdn.tgdd.vn/News/1370564/200-hinh-nen-powerpoint-cute-sieu-de-thuong-day-phong-cach-2-800x500.jpg"
+
+//Link Ảnh Avatar
+ImgLink_Avatar = "https://i.postimg.cc/W4s3hFCM/LAm.png"
+
+//Nickname
+Nickname_Profile = "Nguyễn Ngọc Hoàng Lam ☑ "
+
+//Tên Thật
+RealName_Profile = "Khoa Kinh Tế - Quản Trị"
+
+//Các Link Của Bạn
+LinkInformation = [
+    Facebook_LinkProfile = "https://www.facebook.com/profile.php?id=100024233602692",
+    Discord_LinkProfile = "#",
+    TikTok_LinkProfile = "#",
+    Instagram_LinkProfile = "#"
+]
